65abf16c357c3793fc6c4b15b7d4bbfbf007ed6a
Johannes Zeller <jo.zeller@protonmail.com>
2024-09-14 16:47:50 +0200

    use much more readable string template

--- src/main/java/com/jzel/ollamadebatepoc/ChatConfig.java
+++ src/main/java/com/jzel/ollamadebatepoc/ChatConfig.java
@@ -27,12 +27,12 @@ class ChatConfig {
     final String oppositeSide = debateSide.equals(CONSERVATIVE) ? LIBERAL : CONSERVATIVE;
     return builder
         .defaultSystem(
-            """
-                You are a %s debater.
-                Provide strong, well-reasoned %s arguments and challenge %s views effectively.
-                As an experiment, you are going to face a LLM with the same initial prompt but with the exact opposite regarding the %s spectrum.
-                The goal is to provide the user with an engaging factual discussion by also refuting your opponent’s arguments. Normally it is always important to consider both sides, but you should really only consider the %s side because the other model will do exactly the same thing as a %s.
-                """.formatted(debateSide, debateSide, oppositeSide, debateSide, debateSide, oppositeSide)
+            STR."""
+                You are a \{debateSide} debater.
+                Provide strong, well-reasoned \{debateSide} arguments and challenge \{oppositeSide} views effectively.
+                As an experiment, you are going to face a LLM with the same initial prompt but with the exact opposite regarding the \{debateSide}-\{oppositeSide} spectrum.
+                The goal is to provide the user with an engaging factual discussion by also refuting your opponent’s arguments. Normally it is always important to consider both sides, but you should really only consider the \{debateSide} side because the other model will do exactly the same thing as a \{oppositeSide}.
+                """
         )
         .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
         .build();
