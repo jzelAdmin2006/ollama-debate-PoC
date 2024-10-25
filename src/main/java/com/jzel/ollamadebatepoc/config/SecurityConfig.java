bd9c0a81116bed00fb2c9af323e2aee59ad93dfe
Johannes Zeller <jo.zeller@protonmail.com>
2024-10-25 12:33:56 +0200

    configure public instance

--- src/main/java/com/jzel/ollamadebatepoc/config/SecurityConfig.java
+++ src/main/java/com/jzel/ollamadebatepoc/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.jzel.ollamadebatepoc.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +17,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 class SecurityConfig {
+
+  private final ConfigProperties configProperties;
 
   @Bean
   SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
@@ -28,11 +34,16 @@ class SecurityConfig {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+    configuration.setAllowedOrigins(Arrays.asList(configProperties.allowedOrigins().split(";")));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
     configuration.setAllowedHeaders(List.of("content-type", "x-requested-with", "authorization"));
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
+  }
+
+  @ConfigurationProperties(prefix = "security")
+  record ConfigProperties(String allowedOrigins) {
+
   }
 }
