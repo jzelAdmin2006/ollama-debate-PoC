97a01278151709bb7cb873ba7116dafb4da9c226
Johannes Zeller <jo.zeller@protonmail.com>
2024-11-16 20:20:13 +0100

    reimplement server-side validations

--- src/main/java/com/jzel/ollamadebatepoc/adapter/model/DebateRequest.java
+++ src/main/java/com/jzel/ollamadebatepoc/adapter/model/DebateRequest.java
@@ -1,5 +1,10 @@
 package com.jzel.ollamadebatepoc.adapter.model;
 
-public record DebateRequest(String input, int exchanges, String sessionId) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DebateRequest(@NotBlank @Size(max = 1000) String input, @Min(1) @Max(5) int exchanges, String sessionId) {
 
 }
--- src/main/java/com/jzel/ollamadebatepoc/adapter/ws/DebateController.java
+++ src/main/java/com/jzel/ollamadebatepoc/adapter/ws/DebateController.java
@@ -2,6 +2,7 @@ package com.jzel.ollamadebatepoc.adapter.ws;
 
 import com.jzel.ollamadebatepoc.adapter.model.DebateRequest;
 import com.jzel.ollamadebatepoc.service.DebateService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -15,7 +16,7 @@ class DebateController {
   private final SimpMessagingTemplate messagingTemplate;
 
   @MessageMapping("/debate")
-  public void handleDebate(final DebateRequest request) {
+  public void handleDebate(final @Valid DebateRequest request) {
     messagingTemplate.convertAndSend("/topic/debate/" + request.sessionId(),
         debateService.conductDebate(request.input(), request.exchanges())
     );
